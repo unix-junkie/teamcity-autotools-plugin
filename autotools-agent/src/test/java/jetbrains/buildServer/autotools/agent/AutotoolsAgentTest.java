6d566e33d009b944cc1695a13686f9251db7539f
Nadezhda Demina <nadena.xq95@mail.ru>
2017-08-15 16:55:31 +0300

    add unit tests findVersionTest, getDejagnuParametersTest, stringArrayToStringTest

--- autotools-agent/src/test/java/jetbrains/buildServer/autotools/agent/AutotoolsAgentTest.java
+++ autotools-agent/src/test/java/jetbrains/buildServer/autotools/agent/AutotoolsAgentTest.java
@@ -3,12 +3,13 @@ package jetbrains.buildServer.autotools.agent; /**
  * Author     : Nadezhda Demina
  */
 
+import com.intellij.openapi.util.Pair;
 import java.io.File;
-import jetbrains.buildServer.autotools.agent.AutotoolsTestsReporter;
-import jetbrains.buildServer.autotools.agent.AutotoolsLifeCycleListener;
+import java.util.ArrayList;
+import java.util.List;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
+import static jetbrains.buildServer.autotools.common.AutotoolsBuildConstants.*;
 
 
 /**
@@ -23,15 +24,60 @@ public class AutotoolsAgentTest {
     Assert.assertTrue(AutotoolsTestsReporter.isThisExtensionFile(new File("for.test.log"), ".log"));
   }
 
-
   @Test
-  public void compareVersionsTest(){
-    Assert.assertFalse(AutotoolsLifeCycleListener.compareVersions("1.1", "1"));
+  public void findVersionTest(){
+    String text = "Expect version is       5.45\n" +
+                  "Tcl version is          8.5\n" +
+                  "Framework version is    1.4.4";
+    Assert.assertEquals((new RuntestToolProvider("runtest", "--version")).findVersion(text), "1.4.4");
+
+    text = "Expect version is       5.45\n" +
+           "Tcl version is          8.6\n" +
+           "Framework version is    1.5.1";
+    Assert.assertEquals((new RuntestToolProvider("runtest", "--version")).findVersion(text), "1.5.1");
+
+    text = "Expect version is       5.45\n" +
+           "Tcl version is          8.6\n" +
+           "Framework version is    1.5.3";
+    Assert.assertEquals((new RuntestToolProvider("runtest", "--version")).findVersion(text), "1.5.3");
+
+    text = "DejaGnu version 1.6\n" +
+           "Expect version  5.45\n" +
+           "Tcl version     8.6";
+    Assert.assertEquals((new RuntestToolProvider("runtest", "--version")).findVersion(text), "1.6");
+
   }
 
+  @Test
+  public void getDejagnuParametersTest(){
+    List<Pair<String, String>> answer = new ArrayList<Pair<String, String>>();
+    answer.add(new Pair<String, String>(HAS_RUNTEST_VAR, "1"));
+    answer.add(new Pair<String, String>(MY_RUNTESTFLAGS, "RUNTESTFLAGS=--all"));
+    Assert.assertEquals(RuntestToolProvider.getDejagnuParameters("1.4.4"), answer,"for version 1.4.4");
+    answer.clear();
+
+    answer.add(new Pair<String, String>(HAS_RUNTEST_VAR, "1"));
+    answer.add(new Pair<String, String>(MY_RUNTESTFLAGS, "RUNTESTFLAGS=--all --xml"));
+    Assert.assertEquals(RuntestToolProvider.getDejagnuParameters("1.5.1"), answer, "for version 1.5.1");
+    answer.clear();
+
+    answer.add(new Pair<String, String>(HAS_RUNTEST_VAR, "1"));
+    //answer.add(new Pair<String, String>(MY_RUNTESTFLAGS, "'--all --xml'"));
+    answer.add(new Pair<String, String>(MY_RUNTESTFLAGS, "RUNTESTFLAGS=--all --xml=\"testresults.xml\""));
+    Assert.assertEquals(RuntestToolProvider.getDejagnuParameters("1.5.3"), answer, "for version 1.5.3");
+    Assert.assertEquals(RuntestToolProvider.getDejagnuParameters("1.6"), answer, "for version 1.6");
+  }
   @Test
   public void getRelativePathTest(){
    Assert.assertEquals(new AutotoolsTestsReporter("C:/Users/naduxa/someprojects/teamcity-autotools-plugin/autotools-agent/src/test").getRelativePath("C:/Users/naduxa/someprojects/teamcity-autotools-plugin/autotools-agent/src/test/resources/testng.xml"),
                         "/resources/testng.xml");
+  }
+
+  @Test
+  public void stringArrayToStringTest(){
+    String text = "DejaGnu version 1.6\n" +
+           "Expect version  5.45\n" +
+           "Tcl version     8.6";
+    Assert.assertEquals(AutotoolsToolProvider.stringArrayToString(text.split("\n")), text);
   }
 }
