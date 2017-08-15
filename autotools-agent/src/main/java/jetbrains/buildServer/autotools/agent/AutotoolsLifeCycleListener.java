e2155adccc96fd1679d6130baef3462bbd15b6e0
Nadezhda Demina <nadena.xq95@mail.ru>
2017-08-15 16:33:51 +0300

    remove findToolRuntest and  compareVersions from AutotoolsLifeCycleListener

--- autotools-agent/src/main/java/jetbrains/buildServer/autotools/agent/AutotoolsLifeCycleListener.java
+++ autotools-agent/src/main/java/jetbrains/buildServer/autotools/agent/AutotoolsLifeCycleListener.java
@@ -28,53 +28,11 @@ public class AutotoolsLifeCycleListener extends AgentLifeCycleAdapter {
   }
 
 
-  /**
-   * Returns true if version v1 < version v2.
-   * @param v1 first version
-   * @param v2 second version
-   * @return true if version v1 < version, v2 else false
-   */
-  @NotNull
-  @VisibleForTesting
-  static boolean compareVersions(@NotNull final String v1,@NotNull final String v2){
-    final String[] v1mas = v1.split(".");
-    final String[] v2mas = v2.split(".");
-    for (int i = 0; i < Math.min(v1mas.length, v2mas.length); i++){
-      if (Integer.parseInt(v1mas[i]) != Integer.parseInt(v2mas[i])){
-        return Integer.parseInt(v1mas[i]) < Integer.parseInt(v2mas[i]);
-      }
-    }
-    return v1mas.length < v2mas.length;
-  }
-
-  /**
-   * Find runtest tool
-   * @param runner BuildRunnerContext
-   */
-  private static void findToolRuntest(@NotNull final BuildRunnerContext runner){
-    final GeneralCommandLine commandLine = new GeneralCommandLine();
-    commandLine.setExePath("runtest");
-    commandLine.addParameter("--version");
-    final ExecResult execResult = SimpleCommandLineProcessRunner.runCommand(commandLine, (byte[])null);
-    if (execResult.getExitCode() != 0) {
-      return;
-    }
-    String version = "";
-    for (final String line : execResult.getOutLines()) {
-      if (!line.startsWith("DejaGnu version")) continue;
-      Matcher matcher = Pattern.compile("\\d+(\\.\\d+)+").matcher(line);
-      if (matcher.find()) {
-        version = matcher.group();
-      }
-    }
-    runner.addRunnerParameter(HAS_RUNTEST_VAR, "1");
-    runner.addRunnerParameter(RUNTEST_XML_FILE_VAR, Boolean.toString(compareVersions(version, "1.6")));
   }
 
   @Override
   public void beforeRunnerStart(@NotNull final BuildRunnerContext runner) {
-    findToolRuntest(runner);
-    testReporter = new AutotoolsTestsReporter(System.currentTimeMillis(), runner.getBuild().getBuildLogger(),
+    myRuntestToolProvider = (RuntestToolProvider) myProvidersRegistry.findToolProvider(TOOL_RUNTEST);
                                               runner.getBuild().getCheckoutDirectory().getAbsolutePath() + "/",
                                               Boolean.parseBoolean(runner.getRunnerParameters().get(UI_NEED_DEJAGNU_VALID_XML)));
     testReporter.findDejagnu(runner.getBuild().getCheckoutDirectory());
